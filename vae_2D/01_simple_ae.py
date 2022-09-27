fb44c72325186cffed6604e4dc5a6b871d2515f3
Przemek Pirog <przemekpirog@o2.pl>
2022-09-27 23:25:35 +0200

    initial commit

--- vae_2D/01_simple_ae.py
+++ vae_2D/01_simple_ae.py
@@ -3,6 +3,24 @@ import numpy as np
 from tensorflow import keras
 import matplotlib.pyplot as plt
 
+
+def plot_image(image):
+    plt.imshow(image, cmap='binary')
+    plt.axis("off")
+
+
+def show_reconstructions(model, x_valid, n_images=5):
+    reconstructions = model.predict(x_valid[:n_images])
+    reconstructions = reconstructions * 255  #
+    x_valid = x_valid * 255
+    fig = plt.figure(figsize=(n_images * 1.5, 3))
+    for image_index in range(n_images):
+        plt.subplot(2, n_images, 1 + image_index)
+        plot_image((x_valid[image_index]))
+        plt.subplot(2, n_images, 1 + n_images + image_index)
+        plot_image(reconstructions[image_index])
+    plt.show()
+
 if __name__ == "__main__":
 
     (x_train, y_train),(x_valid,y_valid) = keras.datasets.mnist.load_data()
@@ -13,12 +31,12 @@ if __name__ == "__main__":
     print('X_valid:  ' + str(x_valid.shape))
     print('Y_valid:  ' + str(y_valid.shape))
 
-    from matplotlib import pyplot
+
 
     for i in range(9):
-        pyplot.subplot(330 + 1 + i)
-        pyplot.imshow(x_train[i], cmap=pyplot.get_cmap('gray'))
-    pyplot.show()
+        plt.subplot(330 + 1 + i)
+        plt.imshow(x_train[i], cmap=plt.get_cmap('gray'))
+    plt.show()
 
 
     x_train = np.expand_dims(x_train, -1).astype("float32") / 255
@@ -44,23 +62,9 @@ if __name__ == "__main__":
     ae.summary()
     ae.compile(loss="binary_crossentropy",
                optimizer=keras.optimizers.Adam(learning_rate=0.001))
-    history=ae.fit(x_train,x_train,epochs=10,
+    history=ae.fit(x_train,x_train,epochs=50,
            validation_data=[x_valid,x_valid])
 
-    def plot_image(image):
-        plt.imshow(image,cmap='binary')
-        plt.axis("off")
 
-    def show_reconstructions (model,x_valid, n_images=5):
-        reconstructions=model.predict(x_valid[:n_images])
-        reconstructions=reconstructions*255 #
-        x_valid=x_valid*255
-        fig=plt.figure(figsize=(n_images*1.5,3))
-        for image_index in range(n_images):
-            plt.subplot(2,n_images,1+image_index)
-            plot_image((x_valid[image_index]))
-            plt.subplot(2,n_images,1+n_images+image_index)
-            plot_image(reconstructions[image_index])
-        plt.show()
 
     show_reconstructions(ae,x_valid)
